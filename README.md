fefdacefccd434146444d31318c9069562f038e6
bluespada <pentingmain@gmail.com>
2025-03-09 21:32:20 +0800

    feat: initialize projects setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# INLY
+
+> [!WARNING]
+> This application currently under active development, feel free to make pr or issue if you find an issue.
+
+
+## License
+
+This software is licensed under [MIT](/LICENSE), you can see accompanying file copy or read online at [https://opensource.org/license/mit](https://opensource.org/license/mit).
---
+++ app/src/app.tsx
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Bluespada <pentingmain@gmail.com>
+ * Copyright (c) ExtendThing
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import React from 'react';
+
+export interface AppProps {
+}
+
+const App : React.FC<AppProps> = (props) => {
+    return (<>
+        <h1>Hello</h1>
+    </>);
+}
+
+export default App;
